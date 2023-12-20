d63e1f96efe9437a4a7c86487049f81f89cee98f
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-20 13:31:21 +0200

    book crud validation

--- bookController.js
+++ bookController.js
@@ -1,7 +1,16 @@
 import * as bookService from "./bookService.js"
 import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` })
+
+const validator = z.object({
+    name: z.string().min(3, { message: "The book name should be a string of at least 3 characters." }),
+    pages: z.coerce.number()
+            .min(1, { message: "The number of pages should be a number between 1 and 1000." })
+            .max(1000, { message: "The number of pages should be a number between 1 and 1000."}),
+    isbn: z.string().length(13, { message: "The ISBN should be a string of 13 characters." })
+})
 
 const showForm = async (c) => {
     const data = {
@@ -13,8 +22,18 @@ const showForm = async (c) => {
 
 const addBook = async(c) => {
     const body = await c.req.parseBody()
-    await bookService.addBook(body)
-    return c.redirect("/books")
+    const validationResult = validator.safeParse(body)
+    if(!validationResult.success){
+        return c.html(eta.render("books.eta", {
+            ...body,
+            errors: validationResult.error.format(),
+            books: await bookService.listBooks(),
+        }))
+    } 
+        await bookService.addBook(body)
+        return c.redirect("/books")
+    
+    
 }
 
 const showBook = async(c) => {
